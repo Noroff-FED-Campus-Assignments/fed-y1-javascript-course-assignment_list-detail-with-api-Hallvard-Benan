700326e3c79b7a143a6bd4bef901106094699268
Hallvard-Benan <hallvard.benan@gmail.com>
2023-02-26 16:24:25 +0100

    form validation and form stylling added

--- src/js/contact.js
+++ src/js/contact.js
@@ -4,8 +4,48 @@ Constants
 @example: https://github.com/S3ak/fed-javascript1-api-calls/blob/main/examples/advanced-form.html#L50
 ============================================
 */
-
 // TODO: Get DOM elements from the DOM
+const contactForm = document.getElementById("contact-form");
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const addressInput = document.getElementById("address");
+const subjectInput = document.getElementById("subject");
+const nameError = document.getElementById("name-error");
+const emailError = document.getElementById("email-error");
+const addressError = document.getElementById("address-error");
+const subjectError = document.getElementById("subject-error");
+const validationMessageContainer =
+  document.getElementById("validation-message");
+
+function validateForm(event) {
+  event.preventDefault();
+
+  checkLength(nameInput, 1, nameError);
+  checkLength(addressInput, 25, addressError);
+  checkLength(subjectInput, 10, subjectError);
+  validateEmail(emailInput, emailError);
+}
+contactForm.addEventListener("change", validateForm);
+
+function checkLength(target, requiredLength, errorContainer) {
+  if (target.value.trim().length < requiredLength) {
+    errorContainer.classList.remove("is-hidden");
+  } else {
+    errorContainer.classList.add("is-hidden");
+  }
+}
+
+function validateEmail(email, errorContainer) {
+  const regEx =
+    /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
+  const patternMatches = regEx.test(email.value);
+
+  if (patternMatches !== true) {
+    errorContainer.classList.remove("is-hidden");
+  } else {
+    errorContainer.classList.add("is-hidden");
+  }
+}
 
 // TODO: Create event listeners for the form
 
--- src/js/details.js
+++ src/js/details.js
@@ -1,14 +1,5 @@
-/*
-============================================
-Constants
-@example: https://github.com/S3ak/fed-javascript1-api-calls/blob/main/examples/games.html#L66
-============================================
-*/
-
-// TODO: Get DOM elements from the DOM
 const detailsContainer = document.querySelector(".details-container");
 const title = document.querySelector("#js-h1");
-// TODO: Get the query parameter from the URL
 const queryString = document.location.search;
 
 const params = new URLSearchParams(queryString);
@@ -40,38 +31,3 @@ async function getBeer() {
 }
 
 getBeer();
-// TODO: Get the id from the query parameter
-
-// TODO: Create a new URL with the id @example: https://www.youtube.com/shorts/ps7EkRaRMzs
-
-/*
-============================================
-DOM manipulation
-@example: https://github.com/S3ak/fed-javascript1-api-calls/blob/main/examples/games.html#L89
-============================================
-*/
-
-// TODO: Fetch and Render the lsit to the DOM
-
-// TODO: Create event listeners for the filters and the search
-
-/*
-============================================
-Data fectching
-@example: https://github.com/S3ak/fed-javascript1-api-calls/blob/main/examples/games.html#L104
-============================================
-*/
-
-// TODO: Fetch an a single of objects from the API
-
-/*
-============================================
-Helper functions
-============================================
-*/
-
-/**
- * TODO: Create a function to create a DOM element.
- * @example: https://github.com/S3ak/fed-javascript1-api-calls/blob/main/src/js/detail.js#L36
- * @param {item} item The object with properties from the fetched JSON data.
- */
